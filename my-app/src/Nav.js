38215508c5d0e7c48c3b1a167f7d1177012399a0
venv_jchae <joseph.saunders@inertiallabs.com>
2023-06-24 22:46:04 -0400

    on the end

---
+++ my-app/src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Nav.css';
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
