922baefd4221231cca53a6f97f9e0054b63b1d48
venv_jchae <joseph.saunders@inertiallabs.com>
2023-06-24 23:43:11 -0400

    add java

---
+++ sprint-boot/demo/src/main/java/com/il/demo/entity/SearchInput.java
@@ -0,0 +1,30 @@
+package com.il.demo.entity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class SearchInput {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String query;
+
+    public SearchInput() {
+    }
+
+    public SearchInput(String query) {
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+    public void setQuery(String query) {
+        this.query = query;
+    }
+    // Getters and Setters
+}
