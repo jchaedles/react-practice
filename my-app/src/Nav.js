fe36746a7c142f244039b1662e4564fb9b8c9f38
venv_jchae <joseph.saunders@inertiallabs.com>
2023-06-24 22:55:03 -0400

    fix it

--- my-app/src/Nav.js
+++ my-app/src/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
     return (
         <nav>
             <ul>
-            <li><a href="/"><i className="fas fa-home"></i></a></li>
+                <li><a href="/"><i className="fas fa-home"></i></a></li>
                 <li><a href="/about"><i className="fas fa-info-circle"></i></a></li>
                 <li><a href="/contact"><i className="fas fa-envelope"></i></a></li>
             </ul>
