30d6950214d94a867b9fc5dc753e3af6389badaf
venv_jchae <joseph.saunders@inertiallabs.com>
2023-06-24 22:47:32 -0400

    font awesome

--- my-app/src/Nav.js
+++ my-app/src/Nav.js
@@ -5,9 +5,9 @@ const Nav = () => {
     return (
         <nav>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
+            <li><a href="/"><i className="fas fa-home"></i></a></li>
+                <li><a href="/about"><i className="fas fa-info-circle"></i></a></li>
+                <li><a href="/contact"><i className="fas fa-envelope"></i></a></li>
             </ul>
         </nav>
     );
